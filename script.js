98b53a2f781f9d973d246a5af3a5722a9907a801
maurolps <boroengenharia@gmail.com>
2023-08-08 21:54:32 -0300

    Main Grid created

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-const Container = document.querySelector('#container');
+const mainContainer = document.querySelector('#container');
 const gridContainer = document.querySelector('#grid-container');
+
+function tint(para) {
+  para.target.style.backgroundColor = "pink";
+}
 
 function gridCreate(gridSize, color) {
   for (let i = 0; i < gridSize; i++) {
     const grid = document.createElement('div');
+    grid.className = "grid";
+    grid.id = "grid" + i;
+    grid.style.backgroundColor = "yellow";
     gridContainer.append(grid);
-    grid.style.background = color;
   }
-  for (let i = 1; i < gridSize; i++) {
-  const cloneContainer = gridContainer.cloneNode(true);
-  Container.append(cloneContainer);
+  for (let j = 1; j < gridSize; j++) {
+    const cloneContainer = gridContainer.cloneNode(true);
+    mainContainer.append(cloneContainer);
   }
+  const hoverElement = document.querySelectorAll(".grid");
+  hoverElement.forEach((grids) => {
+    grids.addEventListener('mouseover', tint);
+    });
+
 }
 
 gridCreate(10, 'red');
